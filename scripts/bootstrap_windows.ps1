7d9849a89b420ac39265716a3697f86eaa9fdd47
Justin Benson <jbenson.dev@gmail.com>
2026-06-05 14:06:10 -0700

    Modify PowerToys installation options
    Updated PowerToys installation to be silent and block the welcome screen.

--- scripts/bootstrap_windows.ps1
+++ scripts/bootstrap_windows.ps1
@@ -29,7 +29,8 @@ winget install --id Microsoft.PowerShell -e --accept-package-agreements --accept
 
 # Install Windows Terminal & Utilities
 winget install --id Microsoft.WindowsTerminal -e --accept-package-agreements --accept-source-agreements
-winget install --id Microsoft.PowerToys -e --accept-package-agreements --accept-source-agreements
+# Installs PowerToys silently and completely blocks the first-run welcome splash screen
+winget install --id Microsoft.PowerToys -e --accept-package-agreements --accept-source-agreements --override "--silent --no_start_menu_shortcut --no_welcome" 
 winget install --id voidtools.Everything -e --accept-package-agreements --accept-source-agreements
 
 # Sysinternals Suite
